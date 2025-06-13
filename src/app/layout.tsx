2cbf7ebfc2547ebb5bb71a9d8b5a22fd6ade56b6
Guilhermano Silva <guilhermanodev@gmail.com>
2025-06-13 00:06:58 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next"
+import { Roboto, Roboto_Mono } from "next/font/google"
+import "./globals.css"
+
+const RobotoSans = Roboto({
+  variable: "--font-roboto-sans",
+  subsets: ["latin"],
+});
+
+const RobotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Kanban NextJS",
+  description: "Kanban NextJS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${RobotoSans.variable} ${RobotoMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
